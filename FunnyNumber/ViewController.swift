e40f38ddcb8fb073035cf8d7c871fef303bb90e0
test <asdfg@mail.com>
2018-03-11 11:41:39 +0700

    decease and increase finish

--- FunnyNumber/ViewController.swift
+++ FunnyNumber/ViewController.swift
@@ -10,7 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    Explicit
+    var numberString: String = ""
+    var myClass = MyClass()
     
+    
+    @IBAction func decreaseButtonItem(_ sender: Any) {
+        let showNumberString = myClass.deCreaseNumber(numberString: numberLabel.text!)
+        numberLabel.text = showNumberString
+        
+    }
+    
+    @IBAction func increaseButtonItem(_ sender: Any) {
+        numberString = numberLabel.text!
+        var numberInt = Int(numberString)
+        numberInt = myClass.checkNumber20(numberInt: numberInt!)
+        let showNumberInt = myClass.inCreaseNumber(numberInt: numberInt!)
+        let showNumberString = String(showNumberInt)
+        numberLabel.text = showNumberString
+    
+    }
+    @IBOutlet weak var numberLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
